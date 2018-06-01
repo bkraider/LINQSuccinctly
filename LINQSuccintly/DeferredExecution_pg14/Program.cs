1290d61ea0e7ae680d9a283d4c6ecf2f9d487990
bhanu korremula <bkraider@gmail.com>
2018-06-01 15:34:54 -0400

    Deferred execution

---
+++ LINQSuccintly/DeferredExecution_pg14/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeferredExecution_pg14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacci = {
+                                0,
+                                1,
+                                1,
+                                2,
+                                3,
+                                5
+                            };
+            // Construct the query 
+            IEnumerable<int> numbersGreaterThanTwoQuery = fibonacci.Where(x => x > 2);
+            // At this point the query has been created but not executed 
+            // Change the first element of the input sequence 
+            fibonacci[0] = 99;
+            // Cause the query to be executed (enumerated) 
+            foreach (var number in numbersGreaterThanTwoQuery)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
