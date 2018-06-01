ca32d37f9e7fdf184c842b322060e8a5ea6413c8
bhanu korremula <bkraider@gmail.com>
2018-06-01 16:03:07 -0400

    added array operator and local query

---
+++ LINQSuccintly/LocalQuery_pg17/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LocalQuery_pg17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xml = @"<ingredients> <ingredient name='milk' quantity='200' price='2.99' /> <ingredient name='sugar' quantity='100' price='4.99' /> <ingredient name='safron' quantity='1' price='46.77' /> </ingredients>";
+            XElement xmlData = XElement.Parse(xml);
+            XElement milk = xmlData.Descendants("ingredient").First(x => x.Attribute("name").Value == "milk");
+            XAttribute nameAttribute = milk.FirstAttribute;
+            // name attribute 
+            XAttribute priceAttribute = milk.Attribute("price"); 
+            string priceOfMilk = priceAttribute.Value;
+            // 2.99 
+            XAttribute quantity = milk.Attributes().Skip(1).First();
+            // quantity attribute
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LINQSuccintly/ToArrayOperator_pg15/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToArrayOperator_pg15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacci = {
+                                0,
+                                1,
+                                1,
+                                2,
+                                3,
+                                5
+                            };
+
+            // Construct the query 
+            IEnumerable<int> numbersGreaterThanTwoQuery = fibonacci.Where(x => x > 2).ToArray();
+
+            // At this point the query has been executed because of the .ToArray() 
+            // Change the first element of the input sequence 
+
+            fibonacci[0] = 99;
+
+            // Enumerate the results 
+            foreach (var number in numbersGreaterThanTwoQuery)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
