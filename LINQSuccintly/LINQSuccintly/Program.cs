15a9216ea2df152daa934d476704d072c235b30e
bhanu korremula <bkraider@gmail.com>
2018-06-01 14:12:37 -0400

    second check in

---
+++ LINQSuccintly/LINQSuccintly/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQSuccintly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacci = {
+             0,
+             1,
+             1,
+             2,
+             3,
+             5
+            };
+            // Scalar return value 
+            int numberOfElements = fibonacci.Count();
+            Console.WriteLine("Count: {0}", numberOfElements);
+            // Output sequence return value 
+            IEnumerable<int> distinctNumbers = fibonacci.Distinct();
+            Console.WriteLine("Elements in output sequence:");
+            foreach (var number in distinctNumbers)
+            {
+                Console.WriteLine(number);
+            }
+            Console.ReadLine();
+        }
+    }
+}
