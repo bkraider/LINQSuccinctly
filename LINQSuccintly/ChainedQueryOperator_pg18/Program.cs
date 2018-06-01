a175811864fdef137b0e044ff91e3f591ae943a2
bhanu korremula <bkraider@gmail.com>
2018-06-01 16:53:31 -0400

    Added fluent style and query style samples

---
+++ LINQSuccintly/ChainedQueryOperator_pg18/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainedQueryOperator_pg18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ingredient[] ingredients = {
+                                        new Ingredient {
+                                        Name = "Sugar", Calories = 500
+                                        },
+                                        new Ingredient {
+                                        Name = "Egg", Calories = 100
+                                        },
+                                        new Ingredient {
+                                        Name = "Milk", Calories = 150
+                                        },
+                                        new Ingredient {
+                                        Name = "Flour", Calories = 50
+                                        },
+                                        new Ingredient {
+                                        Name = "Butter", Calories = 200
+                                        }
+                                    };
+
+
+            IEnumerable<string> highCalorieIngredientNamesQuery = 
+                ingredients
+                .Where(x => x.Calories >= 150)
+                .OrderBy(x => x.Name)
+                .Select(x => x.Name);
+
+            foreach (var ingredientName in highCalorieIngredientNamesQuery)
+            {
+                Console.WriteLine(ingredientName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Ingredient
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public int Calories
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ LINQSuccintly/ChainedQueryStyle_pg21/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainedQueryStyle_pg21
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ingredient[] ingredients = {
+                                         new Ingredient {
+                                          Name = "Sugar", Calories = 500
+                                         },
+                                         new Ingredient {
+                                          Name = "Egg", Calories = 100
+                                         },
+                                         new Ingredient {
+                                          Name = "Milk", Calories = 150
+                                         },
+                                         new Ingredient {
+                                          Name = "Flour", Calories = 50
+                                         },
+                                         new Ingredient {
+                                          Name = "Butter", Calories = 200
+                                         }
+                                        };
+
+            IEnumerable<string> highCalorieIngredientNamesQuery = from i in ingredients where i.Calories >= 150 orderby i.Name select i.Name;
+
+            foreach (var ingredientName in highCalorieIngredientNamesQuery)
+            {
+                Console.WriteLine(ingredientName);
+            }
+
+            Console.ReadLine();
+        }
+
+        class Ingredient
+        {
+            public string Name
+            {
+                get;
+                set;
+            }
+            public int Calories
+            {
+                get;
+                set;
+            }
+        }
+    }
+}
