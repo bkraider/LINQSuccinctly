18e65a92bc2cda3ff0e6d9d5b673ef455fcb4421
bhanu korremula <bkraider@gmail.com>
2018-06-02 07:18:55 -0400

    Added into, equijoin and groupjoin

---
+++ LINQSuccintly/EquiJoin_pg25/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquiJoin_pg25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recipe[] recipes = {
+                              new Recipe {
+                               Id = 1, Name = "Mashed Potato"
+                              },
+                              new Recipe {
+                               Id = 2, Name = "Crispy Duck"
+                              },
+                              new Recipe {
+                               Id = 3, Name = "Sachertorte"
+                              }
+                             };
+                                        Review[] reviews = {
+                              new Review {
+                               RecipeId = 1, ReviewText = "Tasty!"
+                              },
+                              new Review {
+                               RecipeId = 1, ReviewText = "Not nice :("
+                              },
+                              new Review {
+                               RecipeId = 1, ReviewText = "Pretty good"
+                              },
+                              new Review {
+                               RecipeId = 2, ReviewText = "Too hard"
+                              },
+                              new Review {
+                               RecipeId = 2, ReviewText = "Loved it"
+                              }
+                             };
+
+            var query = from recipe in recipes
+                        join review in reviews on recipe.Id equals review.RecipeId
+                        select new // anonymous type 
+                        {
+                            RecipeName = recipe.Name,
+                            RecipeReview = review.ReviewText
+                        };
+
+            foreach (var item in query)
+            {
+                Console.WriteLine("{0} - '{1}'", item.RecipeName, item.RecipeReview);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+
+    class Recipe
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+    }
+    class Review
+    {
+        public int RecipeId
+        {
+            get;
+            set;
+        }
+        public string ReviewText
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ LINQSuccintly/IntoRangeVariable_pg24/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntoRangeVariable_pg24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ingredient[] ingredients = {
+                                         new Ingredient {
+                                          Name = "Sugar", Calories = 500
+                                         },
+                                         new Ingredient {
+                                          Name = "Egg", Calories = 100
+                                         },
+                                         new Ingredient {
+                                          Name = "Milk", Calories = 150
+                                         },
+                                         new Ingredient {
+                                          Name = "Flour", Calories = 50
+                                         },
+                                         new Ingredient {
+                                          Name = "Butter", Calories = 200
+                                         }
+};
+            IEnumerable<Ingredient> highCalDairyQuery = from i in ingredients
+                                                        select new
+                                                        {
+                                                            OriginalIngredient = i,
+                                                            IsDairy = i.Name == "Milk" || i.Name == "Butter",
+                                                            IsHighCalorie = i.Calories >= 150
+                                                        }
+            into temp
+                                                        where temp.IsDairy && temp.IsHighCalorie
+                                                        // cannot write "select i;" as into hides the previous range variable i 
+                                                        select temp.OriginalIngredient;
+            foreach (var ingredient in highCalDairyQuery)
+            {
+                Console.WriteLine(ingredient.Name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Ingredient
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public int Calories
+        {
+            get;
+            set;
+        }
+    }
+}
