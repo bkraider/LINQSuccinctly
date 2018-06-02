85604f1fea628c2ecc6ed05930960f20c437499f
bhanu korremula <bkraider@gmail.com>
2018-06-02 05:38:34 -0400

    Added code for let range variable

---
+++ LINQSuccintly/LetRangeVariable/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetRangeVariable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ingredient[] ingredients = {
+                                         new Ingredient {
+                                          Name = "Sugar", Calories = 500
+                                         },
+                                         new Ingredient {
+                                          Name = "Egg", Calories = 100
+                                         },
+                                         new Ingredient {
+                                          Name = "Milk", Calories = 150
+                                         },
+                                         new Ingredient {
+                                          Name = "Flour", Calories = 50
+                                         },
+                                         new Ingredient {
+                                          Name = "Butter", Calories = 200
+                                         }
+};
+            IEnumerable<Ingredient> highCalDairyQuery = from i in ingredients
+                                                        let isDairy = i.Name == "Milk" || i.Name == "Butter"
+                                                        where i.Calories >= 150 && isDairy
+                                                        select i;
+            foreach (var ingredient in highCalDairyQuery)
+            {
+                Console.WriteLine(ingredient.Name);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Ingredient
+    {
+        public string Name
+        {
+            get;
+            set;
+        }
+        public int Calories
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ LINQSuccintly/LetRangeVariable_pg23/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetRangeVariable_pg23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] csvRecipes = {
+                                     "milk,sugar,eggs",
+                                     "flour,BUTTER,eggs",
+                                     "vanilla,ChEEsE,oats"
+                                    };
+            var dairyQuery = from csvRecipe in csvRecipes
+                             let ingredients = csvRecipe.Split(',')
+                             from ingredient in ingredients
+                             let uppercaseIngredient = ingredient.ToUpper()
+                             where uppercaseIngredient == "MILK" || uppercaseIngredient == "BUTTER" || uppercaseIngredient == "CHEESE"
+                             select uppercaseIngredient;
+            foreach (var dairyIngredient in dairyQuery)
+            {
+                Console.WriteLine("{0} is dairy", dairyIngredient);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+}
